28390786ff3f1b1e98d411830a248fd53629fbaf
nigelcardozo <nigelcardozo@gmail.com>
2021-10-26 21:21:54 +0100

    Refatored level selection code

--- lib/routes/LevelSelectionScreen.dart
+++ lib/routes/LevelSelectionScreen.dart
@@ -2,88 +2,69 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'dart:developer';
 
 class LevelSelectionScreen extends StatelessWidget {
+  final int minLevelNumber = 1;
+  final int maxLevelNumber = 20;
+
   const LevelSelectionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>[
-      AppLocalizations.of(context)!.menuLevel + " 1",
-      AppLocalizations.of(context)!.menuLevel + " 2",
-      AppLocalizations.of(context)!.menuLevel + " 3",
-      AppLocalizations.of(context)!.menuLevel + " 4",
-      AppLocalizations.of(context)!.menuLevel + " 5",
-    ];
+    List<String> levelList = generateLevelList(
+        AppLocalizations.of(context)!.menuLevel,
+        minLevelNumber,
+        maxLevelNumber);
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: ListView.builder(
-            padding: const EdgeInsets.all(40),
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                color: Colors.lightGreen,
-                child: ListTile(
-                  leading: LayoutBuilder(builder: (context, constraint) {
-                    return new Icon(Icons.access_alarms,
-                        size: constraint.biggest.height);
-                  }),
-                  // leading: Icon(Icons.account_circle),
-                  title: Text(
-                    entries[index],
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  subtitle:
-                      Text('0 / 15', style: TextStyle(color: Colors.white70)),
-                  onTap: () {
-                    print('Pressed $index');
-                  },
-                ),
-              );
-            }),
-
-        // child: ListView(
-        //   children: [
-        //     Ink(
-        //       color: Colors.lightGreen,
-        //       child: ListTile(
-        //         title: Text('With lightGreen background'),
-        //         onTap: () {
-        //           print('Pressed');
-        //         },
-        //       ),
-        //     ),
-        //   ],
-        // ),
-
-        // child: ListView.builder(
-        //     padding: const EdgeInsets.all(40),
-        //     itemCount: entries.length,
-        //     itemBuilder: (BuildContext context, int index) {
-        //       return Card(
-        //         child: ListTile(
-        //           tileColor: Colors.green,
-        //           focusColor: Colors.greenAccent,
-        //           hoverColor: Colors.blue,
-        //           selectedTileColor: Colors.deepPurple,
-        //           title: Center(child: Text(entries[index])),
-        //           onTap: () {
-        //             print('Pressed');
-        //             //Navigator.of(context).push(MaterialPageRoute(builder: (context)=>FruitDetail(fruitDataModel: Fruitdata[index],)));
-        //           },
-        //         ),
-        //       );
-        //     }),
-      ),
+      body: SafeArea(child: generateListView(levelList)),
     );
   }
+}
+
+List<String> generateLevelList(String menuTitle, int minLevel, int maxLevel) {
+  List<String> levelList = <String>[];
+
+  for (int i = 1; i < 15; i++) {
+    levelList.add(menuTitle + " " + i.toString());
+  }
+
+  return levelList;
+}
+
+ListView generateListView(List<String> entries) {
+  return ListView.builder(
+      padding: const EdgeInsets.all(40),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return generateCard(entries, index, Colors.lightGreen);
+      });
+}
+
+Card generateCard(
+    List<String> entries, int index, MaterialColor requestedColor) {
+  return Card(color: requestedColor, child: generateListTile(entries, index));
+}
+
+ListTile generateListTile(List<String> entries, int index) {
+  return ListTile(
+    leading: LayoutBuilder(builder: (context, constraint) {
+      return new Icon(Icons.access_alarms, size: constraint.biggest.height);
+    }),
+    title: Text(
+      entries[index],
+      style: TextStyle(
+        fontSize: 20.0,
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    subtitle: Text('0 / 15', style: TextStyle(color: Colors.white70)),
+    onTap: () {
+      print('Pressed $index');
+    },
+  );
 }
 
 Flexible generateHeightSpacer(double height) {
