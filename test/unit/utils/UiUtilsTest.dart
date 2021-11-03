3be6d80946e45eed70d2c77a82b54bc95c5c2e01
nigelcardozo <nigelcardozo@gmail.com>
2021-11-03 16:42:02 +0000

    Fixed failing test

--- test/unit/utils/UiUtilsTest.dart
+++ test/unit/utils/UiUtilsTest.dart
@@ -1,13 +1,9 @@
-// Import the test package and Counter class
-import 'package:flutter/material.dart';
 import 'package:quiz_test/utils/UiUtils.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Counter value should be incremented', () {
-    final uiUtils = UiUtils();
-
-    int response = uiUtils.addition(5, 2);
+    int response = addition(5, 2);
 
     expect(response, 7);
   });
