4cc1b2e622d7f826fb35b385b03ad6b82d35bc58
nigelcardozo <nigelcardozo@gmail.com>
2021-11-06 11:23:31 +0000

    Added level test

---
+++ test/unit/json_helper.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+  String encodeSingleJsonLevelData() {
+    return _encodeJsonLevelData(1, "Level One");
+  }
+
+  String _encodeJsonLevelData(int id, String name) {
+    var body = {};
+    body["id"] = id;
+    body["name"] = name;
+    var item = {};
+    item["item"] = body;
+
+    return json.encode(item);
+  }
+}
---
+++ test/unit/models/level_test.dart
@@ -0,0 +1,11 @@
+import 'package:quiz_test/models/Level.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Test that correct id and name are returned', () {
+    Level level = Level(id: 1, name: "Level One");
+
+    expect(level.id, 1);
+    expect(level.name, "Level One");
+  });
+}
